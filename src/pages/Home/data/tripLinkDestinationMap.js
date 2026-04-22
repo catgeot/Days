1f32f744f246d0550e39cdc04586c56d4f72533e
Catgeot <catgeot@gmail.com>
2026-04-22 19:53:55 +0900

    feat: 트립링크 패키지 13개 추가 및 여행지 매핑 확장

--- src/pages/Home/data/tripLinkDestinationMap.js
+++ src/pages/Home/data/tripLinkDestinationMap.js
@@ -8,13 +8,24 @@ const PACKAGE_MAP = {
   'philippines': TRIPLINK_PACKAGES.family[3], // 필리핀 (qi88tt)
   'thailand': TRIPLINK_PACKAGES.family[4], // 태국 (ynrf5y)
   'china': TRIPLINK_PACKAGES.family[5], // 북경 (1c9qfq)
+  'hongkong': TRIPLINK_PACKAGES.family[6], // 홍콩/마카오 (1y2ntm)
+  'taiwan': TRIPLINK_PACKAGES.family[7], // 대만 (o8qb0s)
+  'tokyo': TRIPLINK_PACKAGES.family[8], // 도쿄 (xp28zi)
+  'osaka': TRIPLINK_PACKAGES.family[9], // 오사카/간사이 (6fr41e)
+  'fukuoka': TRIPLINK_PACKAGES.family[10], // 쿠슈/후쿠오카 (1emhcj)
+  'honshu': TRIPLINK_PACKAGES.family[11], // 혼슈 (5pxm7h)
+  'tsushima': TRIPLINK_PACKAGES.family[12], // 대마도 (06r8p6)
   'westeurope': TRIPLINK_PACKAGES.longhaul[0], // 서유럽 (wx9egs)
-  'easteurope': TRIPLINK_PACKAGES.longhaul[1], // 동유럽 (8zfodz)
+  'easteurope': TRIPLINK_PACKAGES.longhaul[1], // 동유럽 (74gnjs)
   'india': TRIPLINK_PACKAGES.longhaul[2], // 인도 (ja02dx)
+  'northeurope': TRIPLINK_PACKAGES.longhaul[3], // 북유럽 (rpc4cq)
+  'africa': TRIPLINK_PACKAGES.longhaul[4], // 아프리카 (3k2cki)
+  'mongolia': TRIPLINK_PACKAGES.longhaul[5], // 몽골/내몽골 (23krvj)
   'guam': TRIPLINK_PACKAGES.resort[0], // 괌/사이판 (1c4mmw)
   'malaysia': TRIPLINK_PACKAGES.resort[1], // 코타키나발루 (nnpyr1)
   'indonesia': TRIPLINK_PACKAGES.resort[2], // 발리/인도네시아 (4uxv69)
   'laos': TRIPLINK_PACKAGES.resort[3], // 라오스 (jd3bcv)
+  'okinawa': TRIPLINK_PACKAGES.resort[4], // 오키나와 (561up2)
 };
 
 export const DESTINATION_PACKAGE_MAP = {
@@ -34,6 +45,57 @@ export const DESTINATION_PACKAGE_MAP = {
   '북해도': PACKAGE_MAP['hokkaido'],
   'sapporo': PACKAGE_MAP['hokkaido'],
   'hokkaido': PACKAGE_MAP['hokkaido'],
+
+  // 일본 (도쿄)
+  '도쿄': PACKAGE_MAP['tokyo'],
+  '동경': PACKAGE_MAP['tokyo'],
+  'tokyo': PACKAGE_MAP['tokyo'],
+
+  // 일본 (오사카/간사이)
+  '오사카': PACKAGE_MAP['osaka'],
+  '교토': PACKAGE_MAP['osaka'],
+  '나라': PACKAGE_MAP['osaka'],
+  '고베': PACKAGE_MAP['osaka'],
+  'osaka': PACKAGE_MAP['osaka'],
+  'kyoto': PACKAGE_MAP['osaka'],
+  'nara': PACKAGE_MAP['osaka'],
+  'kobe': PACKAGE_MAP['osaka'],
+
+  // 일본 (쿠슈/후쿠오카)
+  '후쿠오카': PACKAGE_MAP['fukuoka'],
+  '복강': PACKAGE_MAP['fukuoka'],
+  '규슈': PACKAGE_MAP['fukuoka'],
+  '구슈': PACKAGE_MAP['fukuoka'],
+  '큐슈': PACKAGE_MAP['fukuoka'],
+  '나가사키': PACKAGE_MAP['fukuoka'],
+  '구마모토': PACKAGE_MAP['fukuoka'],
+  'fukuoka': PACKAGE_MAP['fukuoka'],
+  'kyushu': PACKAGE_MAP['fukuoka'],
+  'nagasaki': PACKAGE_MAP['fukuoka'],
+  'kumamoto': PACKAGE_MAP['fukuoka'],
+
+  // 일본 (혼슈)
+  '혼슈': PACKAGE_MAP['honshu'],
+  '히로시마': PACKAGE_MAP['honshu'],
+  '가나자와': PACKAGE_MAP['honshu'],
+  '요코하마': PACKAGE_MAP['honshu'],
+  'honshu': PACKAGE_MAP['honshu'],
+  'hiroshima': PACKAGE_MAP['honshu'],
+  'kanazawa': PACKAGE_MAP['honshu'],
+  'yokohama': PACKAGE_MAP['honshu'],
+
+  // 일본 (대마도)
+  '대마도': PACKAGE_MAP['tsushima'],
+  '쓰시마': PACKAGE_MAP['tsushima'],
+  'tsushima': PACKAGE_MAP['tsushima'],
+
+  // 일본 (오키나와)
+  '오키나와': PACKAGE_MAP['okinawa'],
+  '나하': PACKAGE_MAP['okinawa'],
+  '이시가키': PACKAGE_MAP['okinawa'],
+  'okinawa': PACKAGE_MAP['okinawa'],
+  'naha': PACKAGE_MAP['okinawa'],
+  'ishigaki': PACKAGE_MAP['okinawa'],
 
   // 싱가포르
   '싱가포르': PACKAGE_MAP['singapore'],
@@ -64,6 +126,25 @@ export const DESTINATION_PACKAGE_MAP = {
   '상해': PACKAGE_MAP['china'],
   'beijing': PACKAGE_MAP['china'],
   'shanghai': PACKAGE_MAP['china'],
+
+  // 홍콩/마카오
+  '홍콩': PACKAGE_MAP['hongkong'],
+  '마카오': PACKAGE_MAP['hongkong'],
+  '홍콩섬': PACKAGE_MAP['hongkong'],
+  'hong kong': PACKAGE_MAP['hongkong'],
+  'hongkong': PACKAGE_MAP['hongkong'],
+  'macau': PACKAGE_MAP['hongkong'],
+  'macao': PACKAGE_MAP['hongkong'],
+
+  // 대만
+  '대만': PACKAGE_MAP['taiwan'],
+  '타이베이': PACKAGE_MAP['taiwan'],
+  '타이중': PACKAGE_MAP['taiwan'],
+  '가오슝': PACKAGE_MAP['taiwan'],
+  'taiwan': PACKAGE_MAP['taiwan'],
+  'taipei': PACKAGE_MAP['taiwan'],
+  'taichung': PACKAGE_MAP['taiwan'],
+  'kaohsiung': PACKAGE_MAP['taiwan'],
 
   // 인도
   '인도': PACKAGE_MAP['india'],
@@ -115,10 +196,61 @@ export const DESTINATION_PACKAGE_MAP = {
   '빈': PACKAGE_MAP['easteurope'],
   '크로아티아': PACKAGE_MAP['easteurope'],
   '두브로브니크': PACKAGE_MAP['easteurope'],
+  '자그레브': PACKAGE_MAP['easteurope'],
+  '바르샤바': PACKAGE_MAP['easteurope'],
   'prague': PACKAGE_MAP['easteurope'],
   'budapest': PACKAGE_MAP['easteurope'],
   'vienna': PACKAGE_MAP['easteurope'],
   'dubrovnik': PACKAGE_MAP['easteurope'],
+  'zagreb': PACKAGE_MAP['easteurope'],
+  'warsaw': PACKAGE_MAP['easteurope'],
+
+  // 북유럽
+  '노르웨이': PACKAGE_MAP['northeurope'],
+  '스웨덴': PACKAGE_MAP['northeurope'],
+  '덴마크': PACKAGE_MAP['northeurope'],
+  '핀란드': PACKAGE_MAP['northeurope'],
+  '오슬로': PACKAGE_MAP['northeurope'],
+  '스톡홀름': PACKAGE_MAP['northeurope'],
+  '코펜하겐': PACKAGE_MAP['northeurope'],
+  '헬싱키': PACKAGE_MAP['northeurope'],
+  '레이캬비크': PACKAGE_MAP['northeurope'],
+  'norway': PACKAGE_MAP['northeurope'],
+  'sweden': PACKAGE_MAP['northeurope'],
+  'denmark': PACKAGE_MAP['northeurope'],
+  'finland': PACKAGE_MAP['northeurope'],
+  'oslo': PACKAGE_MAP['northeurope'],
+  'stockholm': PACKAGE_MAP['northeurope'],
+  'copenhagen': PACKAGE_MAP['northeurope'],
+  'helsinki': PACKAGE_MAP['northeurope'],
+  'reykjavik': PACKAGE_MAP['northeurope'],
+  'iceland': PACKAGE_MAP['northeurope'],
+
+  // 아프리카
+  '케냐': PACKAGE_MAP['africa'],
+  '탄자니아': PACKAGE_MAP['africa'],
+  '남아프리카공화국': PACKAGE_MAP['africa'],
+  '이집트': PACKAGE_MAP['africa'],
+  '모로코': PACKAGE_MAP['africa'],
+  '나이로비': PACKAGE_MAP['africa'],
+  '카이로': PACKAGE_MAP['africa'],
+  '마라케시': PACKAGE_MAP['africa'],
+  'kenya': PACKAGE_MAP['africa'],
+  'tanzania': PACKAGE_MAP['africa'],
+  'south africa': PACKAGE_MAP['africa'],
+  'egypt': PACKAGE_MAP['africa'],
+  'morocco': PACKAGE_MAP['africa'],
+  'nairobi': PACKAGE_MAP['africa'],
+  'cairo': PACKAGE_MAP['africa'],
+  'marrakech': PACKAGE_MAP['africa'],
+
+  // 몽골/내몽골
+  '몽골': PACKAGE_MAP['mongolia'],
+  '울란바토르': PACKAGE_MAP['mongolia'],
+  '내몽골': PACKAGE_MAP['mongolia'],
+  'mongolia': PACKAGE_MAP['mongolia'],
+  'ulaanbaatar': PACKAGE_MAP['mongolia'],
+  'inner mongolia': PACKAGE_MAP['mongolia'],
 
   // 남태평양
   '괌': PACKAGE_MAP['guam'],
@@ -139,8 +271,8 @@ export const FALLBACK_PACKAGE_MAP = {
   'europe': PACKAGE_MAP['westeurope'],
   '베트남': PACKAGE_MAP['vietnam'],
   'vietnam': PACKAGE_MAP['vietnam'],
-  '일본': PACKAGE_MAP['hokkaido'], // 임시로 홋카이도 패키지로 연결
-  'japan': PACKAGE_MAP['hokkaido'],
+  '일본': PACKAGE_MAP['tokyo'], // 도쿄를 기본 일본 패키지로
+  'japan': PACKAGE_MAP['tokyo'],
   '말레이시아': PACKAGE_MAP['malaysia'],
   'malaysia': PACKAGE_MAP['malaysia'],
   '필리핀': PACKAGE_MAP['philippines'],
@@ -154,5 +286,7 @@ export const FALLBACK_PACKAGE_MAP = {
   '인도네시아': PACKAGE_MAP['indonesia'],
   'indonesia': PACKAGE_MAP['indonesia'],
   '라오스': PACKAGE_MAP['laos'],
-  'laos': PACKAGE_MAP['laos']
+  'laos': PACKAGE_MAP['laos'],
+  '아프리카': PACKAGE_MAP['africa'],
+  'africa': PACKAGE_MAP['africa']
 };
--- src/pages/Home/data/tripLinkPackages.js
+++ src/pages/Home/data/tripLinkPackages.js
@@ -66,6 +66,83 @@ export const TRIPLINK_PACKAGES = {
       description: "역사와 문화가 살아 숨쉬는 곳",
       width: 1024,
       height: 768
+    },
+    {
+      id: "dyn-banner-family-7",
+      type: "iframe",
+      adKey: "1y2ntm", // 모달 (1024x768)
+      bannerAdKey: "qab4a8", // 배너 (728x90)
+      targetKeyword: "Hong Kong",
+      title: "홍콩/마카오",
+      description: "동서양의 매력이 공존하는 도시",
+      width: 1024,
+      height: 768
+    },
+    {
+      id: "dyn-banner-family-8",
+      type: "iframe",
+      adKey: "o8qb0s", // 모달 (1024x768)
+      bannerAdKey: "0gyv4b", // 배너 (728x90)
+      targetKeyword: "Taiwan",
+      title: "대만",
+      description: "가까운 이국, 맛과 감성의 여행",
+      width: 1024,
+      height: 768
+    },
+    {
+      id: "dyn-banner-family-9",
+      type: "iframe",
+      adKey: "xp28zi", // 모달 (1024x768)
+      bannerAdKey: "z6vosf", // 배너 (728x90)
+      targetKeyword: "Tokyo",
+      title: "도쿄",
+      description: "일본 수도의 현대와 전통",
+      width: 1024,
+      height: 768
+    },
+    {
+      id: "dyn-banner-family-10",
+      type: "iframe",
+      adKey: "6fr41e", // 모달 (1024x768)
+      bannerAdKey: "g4wvr7", // 배너 (728x90)
+      targetKeyword: "Osaka",
+      title: "오사카/간사이",
+      description: "맛과 재미의 일본 서부",
+      width: 1024,
+      height: 768
+    },
+    {
+      id: "dyn-banner-family-11",
+      type: "iframe",
+      adKey: "1emhcj", // 모달 (1024x768)
+      bannerAdKey: "gvlbzg", // 배너 (728x90)
+      targetKeyword: "Fukuoka",
+      title: "쿠슈/후쿠오카",
+      description: "가까운 일본 규슈 여행",
+      width: 1024,
+      height: 768
+    },
+    {
+      id: "dyn-banner-family-12",
+      type: "iframe",
+      adKey: "5pxm7h", // 모달 (1024x768)
+      bannerAdKey: "2bsj97", // 배너 (728x90)
+      targetKeyword: "Honshu",
+      title: "혼슈",
+      description: "일본 본섬의 다채로운 매력",
+      width: 1024,
+      height: 768
+    },
+    {
+      id: "dyn-banner-family-13",
+      type: "iframe",
+      adKey: "06r8p6", // 모달 (1024x768)
+      bannerAdKey: "kbt6ed", // 배너 (728x90)
+      targetKeyword: "Tsushima",
+      title: "대마도",
+      description: "가까운 일본 섬 여행",
+      width: 1024,
+      height: 768
     }
   ],
   // 테마 2: 전문가와 함께하는, 유럽 & 장거리 일주
@@ -84,10 +161,10 @@ export const TRIPLINK_PACKAGES = {
     {
       id: "dyn-banner-longhaul-2",
       type: "iframe",
-      adKey: "8zfodz", // 모달 (1024x768)
-      bannerAdKey: "q5ejk8", // 배너 (728x90)
+      adKey: "74gnjs", // 모달 (1024x768) - 업데이트된 동유럽 링크
+      bannerAdKey: "p2dnc5", // 배너 (728x90) - 업데이트된 동유럽 링크
       targetKeyword: "Prague",
-      title: "동유럽/발칸 3국",
+      title: "동유럽",
       description: "낭만 가득한 장거리 추천 패키지",
       width: 1024,
       height: 768
@@ -100,6 +177,39 @@ export const TRIPLINK_PACKAGES = {
       targetKeyword: "India",
       title: "인도",
       description: "신비로운 매력의 인도 여행",
+      width: 1024,
+      height: 768
+    },
+    {
+      id: "dyn-banner-longhaul-4",
+      type: "iframe",
+      adKey: "rpc4cq", // 모달 (1024x768)
+      bannerAdKey: "pro3zf", // 배너 (728x90)
+      targetKeyword: "Norway",
+      title: "북유럽",
+      description: "신비로운 오로라와 피오르",
+      width: 1024,
+      height: 768
+    },
+    {
+      id: "dyn-banner-longhaul-5",
+      type: "iframe",
+      adKey: "3k2cki", // 모달 (1024x768)
+      bannerAdKey: "jb2l3k", // 배너 (728x90)
+      targetKeyword: "Africa",
+      title: "아프리카",
+      description: "대자연과 야생의 감동",
+      width: 1024,
+      height: 768
+    },
+    {
+      id: "dyn-banner-longhaul-6",
+      type: "iframe",
+      adKey: "23krvj", // 모달 (1024x768)
+      bannerAdKey: "zqucn5", // 배너 (728x90)
+      targetKeyword: "Mongolia",
+      title: "몽골/내몽골",
+      description: "광활한 초원과 유목 문화",
       width: 1024,
       height: 768
     }
@@ -147,6 +257,17 @@ export const TRIPLINK_PACKAGES = {
       targetKeyword: "Laos",
       title: "라오스",
       description: "순수하고 평화로운 힐링 여행",
+      width: 1024,
+      height: 768
+    },
+    {
+      id: "dyn-banner-resort-5",
+      type: "iframe",
+      adKey: "561up2", // 모달 (1024x768)
+      bannerAdKey: "m3z5qz", // 배너 (728x90)
+      targetKeyword: "Okinawa",
+      title: "오키나와",
+      description: "일본의 열대 휴양 천국",
       width: 1024,
       height: 768
     }
